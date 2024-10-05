fecb0f76f0f64b913f1e3acc23a16d5e8dd6bdfd
Numonovdev <ebxuzsher@gmail.com>
2024-10-05 18:34:54 +0300

    finish

--- src/components/NewCard.jsx
+++ src/components/NewCard.jsx
@@ -1,13 +1,59 @@
-function NewCard(){
+import { useRef } from "react";
+
+function NewCard({onsubmit}){
+     const nameRef=useRef();
+     const descriptionRef=useRef();
+     const priceRef=useRef();
+     const createdatRef=useRef();
+     const updatedatRef=useRef();
+
+
+     function Validate(){
+
+          if(nameRef.current.value.length < 3){
+               alert('name is not ');
+               nameRef.current.focus();
+               nameRef.current.style.outlineColor='red'
+               return false;
+          }
+          return true;
+     }
+     function headleClick(event){
+          event.preventDefault();
+
+          const isvalid = Validate();
+          if(!isvalid){
+               return;
+          }
+
+          const user = {
+               name: nameRef.current.value,
+               description: descriptionRef.current.value,
+               price: priceRef.current.value,
+               createdat: createdatRef.current.value,
+               updatedat: updatedatRef.current.value,
+          }
+onsubmit(user);
+
+clearForm();
+     }
+     function clearForm(){
+          nameRef.current.value='';
+          descriptionRef.current.value="";
+          priceRef.current.value="";
+          createdatRef.current.value="";
+          updatedatRef.current.value="";
+     }
+
      return(
-          <div >
-               <form>
-                    <input type="text" placeholder="Enter name ..." />
-                    <input type="text" placeholder="Enter Description ..." />
-                    <input type="text" placeholder="Enter Price ..." />
-                    <input type="text" placeholder="Enter CreatedAt ..." />
-                    <input type="text" placeholder="Enter UpdatedAt ..." />
-                    <button className="py-1 w-2/3 bg-gray-700 border-none hover:bg-gray-400 text-black font-bold font-base ">Join</button>
+          <div className="absolute w-1/4 z-10 mt-10 ml-[950px]">
+               <form className="flex gap-1 flex-col items-center">
+                    <input ref={nameRef}  className="py-1 px-2 outline-none" type="text" placeholder="Enter name ..." />
+                    <input ref={descriptionRef} className="py-1 px-2 outline-none" type="text" placeholder="Enter Description ..." />
+                    <input ref={priceRef} className="py-1 px-2 outline-none" type="text" placeholder="Enter Price ..." />
+                    <input ref={createdatRef} className="py-1 px-2 outline-none" type="text" placeholder="Enter CreatedAt ..." />
+                    <input ref={updatedatRef} className="py-1 px-2 outline-none" type="text" placeholder="Enter UpdatedAt ..." />
+                    <button onClick={headleClick} className="py-1 w-2/3 bg-gray-700 border-none hover:bg-gray-400 text-black font-bold font-base ">Join</button>
                </form>
           </div>
      )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,28 +4,37 @@ import Card from "../components/Card";
 import NewCard from '../components/NewCard';
 
 function Home() {
+  const [newdata, setNewdata] = useState(null);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-    useEffect(() => {
+    useEffect(() => {  
     fetch("https://auth-rg69.onrender.com/api/products/all")
       .then(res => res.json())
       .then(data => {
-        console.log(data);
         setData(data);
-        setLoading(false);
       })
       .catch(err => {
         console.log(err);
-        setLoading(false);
-           });
+           })
+      .finally(()=>{
+        setLoading(false)
+      })
+      let copy = data;
+      copy = copy.push(newdata);
+      setData(copy)
   }, []);
+  const createdata =(newdata)=>{
+    setNewdata(newdata)
+    setData(prevData=>[...prevData,newdata]);
+  };
 
   return (
-    <div className="flex flex-wrap gap-10 mx-auto max-w-6xl w-full mt-10 relative">{loading ? (<img className='w-1/5 mx-auto' src={gifloading} alt="Loading GIF" width="500" />) 
+    <div className="flex flex-wrap gap-10 mx-auto max-w-6xl w-full mt-10 relative ">{loading ? (<img className='w-1/5 mx-auto' src={gifloading} alt="Loading GIF" width="500" />) 
     :
      (data.length > 0 ? (data.map((value, index) => (<Card data={value} key={index} />))) : ( <p>No products available.</p>
      )
       )}
+      <NewCard onsubmit={createdata} />
     </div>
   );
 }
